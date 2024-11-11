adba87923983c4dab921bdd4b82751d85f808ac9
fluentebidev <alison.pezzott@fluentebi.com>
2024-11-10 23:39:04 -0300

    Criados lake, cotacoes, moedas

---
+++ criar_moedas.Notebook/notebook-content.py
@@ -0,0 +1,71 @@
+# Fabric notebook source
+
+# METADATA ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "synapse_pyspark"
+# META   },
+# META   "dependencies": {
+# META     "lakehouse": {
+# META       "default_lakehouse": "a5db6062-04b4-468e-a142-a2e8f3dd7689",
+# META       "default_lakehouse_name": "lake",
+# META       "default_lakehouse_workspace_id": "ecb4846e-b12c-4369-8df4-c5289492a28d",
+# META       "known_lakehouses": [
+# META         {
+# META           "id": "a5db6062-04b4-468e-a142-a2e8f3dd7689"
+# META         }
+# META       ]
+# META     }
+# META   }
+# META }
+
+# CELL ********************
+
+
+# Importação de bibliiotecas
+from pyspark.sql.types import StructType, StructField, StringType
+
+# Criação do schema do dataframe
+schema = StructType([
+    StructField("Moeda", StringType(), True),
+    StructField("Nome", StringType(), True),
+    StructField("Formato", StringType(), True)
+])
+
+# Dados das moedas
+data = [
+    ("BRL", "Real brasileiro",          "R$ #,##0.00;R$ #,##0.00;-" ),
+    ("AUD", "Dólar australiano",        "$ #,##0.00;$ #,##0.00;-"   ),
+    ("CAD", "Dólar canadense",          "$ #,##0.00;$ #,##0.00;-"   ),
+    ("CHF", "Franco suíço",             "Fr #,##0.00;Fr #,##0.00;-" ),
+    ("DKK", "Coroa dinamarquesa",       "kr #,##0.00;kr #,##0.00;-" ),
+    ("EUR", "Euro",                     "€ #,##0.00;€ #,##0.00;-"   ),
+    ("GBP", "Libra Esterlina",          "£ #,##0.00;£ #,##0.00;-"   ),
+    ("JPY", "Iene",                     "¥ #,##0;¥ #,##0;-"         ),
+    ("NOK", "Coroa norueguesa",         "kr #,##0.00;kr #,##0.00;-" ),
+    ("SEK", "Coroa sueca",              "kr #,##0.00;kr #,##0.00;-" ),
+    ("USD", "Dólar dos Estados Unidos", "$ #,##0.00;$ #,##0.00;-"   )
+]
+
+# Criando o DataFrame
+df = spark.createDataFrame(data, schema=schema)
+
+# Exibindo o dataframe
+# df.show()
+# df.printSchema()
+
+# Escrevendo no lakehouse
+df.write.format("delta").mode("overwrite").saveAsTable("moedas")
+
+# Exibindo o dataframe do lakehouse
+df_lake = spark.sql("SELECT * FROM lake.moedas")
+display(df_lake)
+
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
